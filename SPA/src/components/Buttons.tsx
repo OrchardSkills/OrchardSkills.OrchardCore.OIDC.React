ffeb07b54c93bea9b510287019c0827d7389813b
sales <sales@angularadvisors.com>
2020-10-10 13:00:42 -0600

    Removed debug buttons

--- SPA/src/components/Buttons.tsx
+++ SPA/src/components/Buttons.tsx
@@ -15,15 +15,6 @@ const Buttons: React.FunctionComponent<IButtonsProps> = props => {
         <button className="btn btn-primary btn-login" style={{ margin: '10px' }} onClick={props.login}>
           Login
         </button>
-        <button className="btn btn-secondary btn-getuser" style={{ margin: '10px' }} onClick={props.getUser}>
-          Get User info
-        </button>
-        <button className="btn btn-warning btn-getapi" style={{ margin: '10px' }} onClick={props.callApi}>
-          Call API
-        </button>
-        <button className="btn btn-success btn-renewtoken" style={{ margin: '10px' }} onClick={props.renewToken}>
-          Renew Token
-        </button>
         <button className="btn btn-dark btn-logout" style={{ margin: '10px' }} onClick={props.logout}>
           Logout
         </button>
@@ -33,3 +24,24 @@ const Buttons: React.FunctionComponent<IButtonsProps> = props => {
 };
 
 export default Buttons;
+
+// Debug HTML
+// <div className="row">
+// <div className="col-md-12 text-center" style={{ marginTop: '30px' }}>
+//   <button className="btn btn-primary btn-login" style={{ margin: '10px' }} onClick={props.login}>
+//     Login
+//   </button>
+//   <button className="btn btn-secondary btn-getuser" style={{ margin: '10px' }} onClick={props.getUser}>
+//     Get User info
+//   </button>
+//   <button className="btn btn-warning btn-getapi" style={{ margin: '10px' }} onClick={props.callApi}>
+//     Call API
+//   </button>
+//   <button className="btn btn-success btn-renewtoken" style={{ margin: '10px' }} onClick={props.renewToken}>
+//     Renew Token
+//   </button>
+//   <button className="btn btn-dark btn-logout" style={{ margin: '10px' }} onClick={props.logout}>
+//     Logout
+//   </button>
+// </div>
+// </div>
