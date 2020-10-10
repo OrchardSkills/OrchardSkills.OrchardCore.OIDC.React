e4b5211aad5c13469d94ab687f3f0e704b5840b0
senshi111 <banenicovic@gmail.com>
2020-10-10 17:24:14 +0200

    add api call

--- SPA/src/components/subscriber-list.component.js
+++ SPA/src/components/subscriber-list.component.js
@@ -2,22 +2,44 @@ import React, { Component } from "react";
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import SubscriberTableRow from './SubscriberTableRow';
-
+import { AuthService } from '../services/AuthService';
+import { ApiService } from '../services/ApiService';
+import { Constants } from '../helpers/Constants';
 
 export default class SubscriberList extends Component {
+  authService;
+  apiService;
+  token = '';
 
   constructor(props) {
     super(props)
     this.state = {
       subscribers: []
     };
+    this.authService = new AuthService()
+    this.apiService = new ApiService()
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3000/subscriber/')
+    this.apiService.callApi().then(res => {
+      console.log('data', res.data.data.subscriber)
+
+    })
+    this.authService.getUser().then(user => {
+      this.token = user.access_token
+      console.log('token', this.token)
+      const headers = {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + this.token
+      };
+
+    })
+
+    axios.get(Constants.apiRoot + '?query={subscriber {createdUtc, displayText email firstName lastName modifiedUtc publishedUtc contentItemId }}')
       .then(res => {
+        console.log('data', res)
         this.setState({
-          subscribers: res.data
+          subscribers: res.data.data
         });
       })
       .catch((error) => {
